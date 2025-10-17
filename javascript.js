f2ed4746020b99c85cb8c9a79b6a34cde24c98bf
Nomcebo821 <nomcebonkomo203@gmail.com>
2025-10-17 21:14:48 +0200

    Add new button

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const cartbutton = document.getElementById("cart");
+    cartbutton.addEventListener("click", function(){
+        alert("Cart button clicked");
+    });
+});
+
